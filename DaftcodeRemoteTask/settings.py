753835f2628c3727a7b57f156a6f1cc719a09f39
MichavR <michalrdz@gmail.com>
2021-05-21 22:00:56 +0200

    deploy preps

--- DaftcodeRemoteTask/settings.py
+++ DaftcodeRemoteTask/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jx(bmyzicx&!)^@kxmikc)96(+z_w4=p7c+!jd6i$v+$l=y$-m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
