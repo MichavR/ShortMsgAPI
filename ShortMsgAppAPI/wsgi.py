564dfdad71099e14e0338a2ed7728237ed690677
MichavR <michalrdz@gmail.com>
2021-08-11 23:38:57 +0200

    minor refactor

--- ShortMsgAppAPI/wsgi.py
+++ ShortMsgAppAPI/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for short-msg-api-app project.
+WSGI config for ShortMsgAppAPI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'short-msg-api-app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShortMsgAppAPI.settings')
 
 application = get_wsgi_application()
